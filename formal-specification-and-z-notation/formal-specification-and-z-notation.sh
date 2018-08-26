4a9e32e8d2d02009bf3e36b524b2123665bc1e1c
Arthur Ryman <arthur.ryman@gmail.com>
2018-08-26 12:28:20 -0400

    Refactored Z notation article

--- formal-specification-and-z-notation/formal-specification-and-z-notation.sh
+++ formal-specification-and-z-notation/formal-specification-and-z-notation.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
-#!/usr/bin/env bash
 cat ../shared/fuzzlib\
  ../topological-spaces/topological-spaces.sty\
  ../topological-spaces/topological-spaces.tex\
  ../real-numbers/real-numbers.sty\
  ../real-numbers/real-numbers.tex\
- vector-spaces.sty > vector-spaces.prelude
-fuzz -p vector-spaces.prelude vector-spaces
+ formal-specification-and-z-notation.sty > formal-specification-and-z-notation.prelude
+fuzz -p formal-specification-and-z-notation.prelude formal-specification-and-z-notation
