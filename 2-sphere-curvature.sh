ce4207a56ff715b9dfdb61ea78d703d5f0e4e1fb
Arthur Ryman <arthur.ryman@gmail.com>
2020-04-29 11:32:35 -0400

    Added type checking for main article.

--- 2-sphere-curvature.sh
+++ 2-sphere-curvature.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 cat shared/fuzzlib\
+ sets/sets.sty\
+ sets/sets.tex\
+ topological-spaces/topological-spaces.sty\
+ topological-spaces/topological-spaces.tex\
  real-numbers/real-numbers.sty\
  real-numbers/real-numbers.tex\
  vector-spaces/vector-spaces.sty\
